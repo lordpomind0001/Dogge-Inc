4c732fa54cb95641af0b58a9eb0a58fcf0b4abb6
lordpomind0001 <118119431+lordpomind0001@users.noreply.github.com>
2024-04-22 09:53:21 +0200

    Update message.py

--- Doggel_Inc/DICord/message.py
+++ Doggel_Inc/DICord/message.py
@@ -8,5 +8,5 @@ class Message:
     self.author = User(message_data['author'], client)
     self.channel = Channel(message_data['channel_id'], client)
   
-  async def reply(self, content):
-    await self.channel.send(content, reference=self.id)
+  async def reply(self, content, embed=None):
+    await self.channel.send(content, reference=self.id, embed=embed)
