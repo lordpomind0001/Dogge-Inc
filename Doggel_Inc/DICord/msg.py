afc18770596b693183cae675c92510ee1327d5e5
lordpomind0001 <118119431+lordpomind0001@users.noreply.github.com>
2024-04-22 09:49:17 +0200

    Update msg.py

--- Doggel_Inc/DICord/msg.py
+++ Doggel_Inc/DICord/msg.py
@@ -9,8 +9,8 @@ class Msg:
     self.author = User(message_data['author'], client)
     self.channel = Channel(message_data['channel_id'], client)
     
-  async def send(self, content):
-    await self.channel.send(content)
+  async def send(self, content, embed=None):
+    await self.channel.send(content, embed=embed)
 
-  async def reply(self, content):
-    await self.channel.send(content, reference=self.id)
+  async def reply(self, content, embed=None):
+    await self.channel.send(content, reference=self.id, embed=embed)
