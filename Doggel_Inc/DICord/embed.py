ba2ea43aaf3f77d85f14092f385159828e46f34e
lordpomind0001 <118119431+lordpomind0001@users.noreply.github.com>
2024-04-22 09:59:18 +0200

    Update embed.py

--- Doggel_Inc/DICord/embed.py
+++ Doggel_Inc/DICord/embed.py
@@ -1,1 +1,28 @@
 
+class Embed:
+
+    def create_embed(self, title, description, color, fields=None, footer=None, image_url=None):
+        """
+        Create an embed structure for Discord.
+
+        Parameters:
+        title (str): The title of the embed.
+        description (str): The description of the embed.
+        color (int): The color code of the embed.
+        fields (list of dict): A list of fields to include in the embed.
+        footer (dict): The footer information.
+        image_url (str): The URL of the image to include in the embed.
+
+        Returns:
+        dict: The embed structure.
+        """
+        embed = {
+            'title': title,
+            'description': description,
+            'color': color,
+            'fields': fields if fields else [],
+            'footer': footer if footer else {},
+            'image': {'url': image_url} if image_url else {}
+        }
+        return embed
+  
